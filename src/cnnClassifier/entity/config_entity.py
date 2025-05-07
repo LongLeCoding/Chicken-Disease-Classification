0d5043eefbe603baeea71801a4da20ba9008b8cc
LongLeCoding <168743377+LongLeCoding@users.noreply.github.com>
2025-05-06 21:16:10 -0500

    prepare base model completed

--- src/cnnClassifier/entity/config_entity.py
+++ src/cnnClassifier/entity/config_entity.py
@@ -1,9 +1,26 @@
 from dataclasses import dataclass
 from pathlib import Path
 
+#Stage 01
 @dataclass(frozen=True)
 class DataIngestionConfig:
     root_dir: Path
     source_URL: str
     local_data_file: Path
     unzip_dir: Path
+
+#Stage 02
+@dataclass(frozen=True)
+class PrepareBaseModelConfig:
+    root_dir: Path
+    base_model_path: Path
+    updated_base_model_path: Path
+    params_image_size: list
+    params_learning_rate: list
+    params_include_top: bool
+    params_weights: str
+    params_classes: int
+
+
+#Stage 03
+
