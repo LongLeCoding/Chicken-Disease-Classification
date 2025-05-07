8baa78931e07781960036a39a5897ff8147071b6
LongLeCoding <168743377+LongLeCoding@users.noreply.github.com>
2025-05-06 21:59:36 -0500

    Prepare callbacks added

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,6 +1,11 @@
 from cnnClassifier.constants import *
 from cnnClassifier.utils.common import read_yaml, create_directories
-from cnnClassifier.entity.config_entity import DataIngestionConfig, PrepareBaseModelConfig
+from cnnClassifier.entity.config_entity import DataIngestionConfig, PrepareBaseModelConfig, PrepareCallbacksConfig
+import os
+
+
+
+
 
 class ConfigurationManager:
     def __init__(self, config_filepath = CONFIG_FILE_PATH,params_filepath = PARAMS_FILE_PATH):
@@ -22,8 +27,9 @@ class ConfigurationManager:
             local_data_file = config.local_data_file,
             unzip_dir = config.unzip_dir
         )
-
         return data_ingestion_config
+    
+
     def get_prepare_base_model_config(self) -> PrepareBaseModelConfig:
         config = self.config.prepare_base_model
 
@@ -39,8 +45,24 @@ class ConfigurationManager:
             params_weights= self.params.WEIGHTS,
             params_classes=self.params.CLASSES
         )
-
         return prepare_base_model
+    
+    
+    def get_prepare_callback_config(self) -> PrepareCallbacksConfig:
+        config = self.config.prepare_callbacks
+        model_ckpt_dir = os.path.dirname(config.checkpoint_model_filepath)
+        create_directories([
+            Path(model_ckpt_dir),
+            Path(config.tensorboard_root_log_dir)
+        ])
+
+        prepare_callback_config = PrepareCallbacksConfig(
+            root_dir=Path(config.root_dir),
+            tensorboard_root_log_dir=Path(config.tensorboard_root_log_dir),
+            checkpoint_model_filepath=Path(config.checkpoint_model_filepath)
+        )
+        return prepare_callback_config
+
     
     
 
--- src/cnnClassifier/entity/config_entity.py
+++ src/cnnClassifier/entity/config_entity.py
@@ -23,4 +23,9 @@ class PrepareBaseModelConfig:
 
 
 #Stage 03
+@dataclass(frozen=True)
+class PrepareCallbacksConfig:
+    root_dir : Path
+    tensorboard_root_log_dir : Path
+    checkpoint_model_filepath: Path
 
